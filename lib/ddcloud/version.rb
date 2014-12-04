3d04a8d3da950c6e7034cb2be7f9824bb050b750
smu <stijnmuylle@gmail.com>
2014-12-04 16:04:40 +0100

    version update

--- lib/ddcloud/version.rb
+++ lib/ddcloud/version.rb
@@ -1,5 +1,5 @@
 module DDcloud
-  VERSION = "0.3.1"
+  VERSION = "0.3.2"
 
   {"xmlns:ns3"=>"http://oec.api.opsource.net/schemas/organization",
     "xmlns:ns4"=>"http://oec.api.opsource.net/schemas/network",
